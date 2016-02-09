da884d75449815b4afb5042011b6d1ffe90d6ee0
shin-nuriko <nuriko@Hyacynths-MBP.home>
2016-02-09 15:22:53 +0800

    downloaded version 3.3.1

---
+++ symfony/web/resource_dir_inc.php
@@ -0,0 +1,5 @@
+<?php 
+sfConfig::set('sf_web_css_dir_name','webres_554af33274f2b9.68269152/css');
+sfConfig::set('sf_web_js_dir_name','webres_554af33274f2b9.68269152/js');
+sfConfig::set('sf_web_images_dir_name','webres_554af33274f2b9.68269152/images');
+sfConfig::set('ohrm_resource_dir', 'webres_554af33274f2b9.68269152');
