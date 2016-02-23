5f0f6940dc0de2ab969e2b1abedbc51af47e0c57
shin-nuriko <nuriko@Hyacynths-MBP.home>
2016-02-23 20:03:31 +0800

    initial files for viewPrintDetails

---
+++ symfony/plugins/orangehrmPimPlugin/modules/pim/actions/viewPrintDetailsAction.class.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
+ */
+/**
+ * printDetailsAction
+ *
+ */
+class viewPrintDetailsAction extends basePimAction {
+    private $employee;
+    public $empInfo = array();
+
+
+    /**
+     * Get EmployeeService
+     * @returns EmployeeService
+     */
+    public function getEmployeeService() {
+        if (is_null($this->employeeService)) {
+            $this->employeeService = new EmployeeService();
+            $this->employeeService->setEmployeeDao(new EmployeeDao());
+        }
+        return $this->employeeService;
+    }
+    
+    public function execute($request) {
+
+        $loggedInEmpNum = $this->getUser()->getEmployeeNumber();
+
+        $personal = $request->getParameter('personal');
+        $empNumber = (isset($personal['txtEmpID']))?$personal['txtEmpID']:$request->getParameter('empNumber');
+        $this->empNumber = $empNumber;
+
+        $this->personalInformationPermission = $this->getDataGroupPermissions('personal_information', $empNumber);
+
+
+        if (!$this->IsActionAccessible($empNumber)) {
+            $this->forward(sfConfig::get('sf_secure_module'), sfConfig::get('sf_secure_action'));
+        }
+
+        if ($this->personalInformationPermission->canUpdate()){
+                $this->employee = $this->getEmployeeService()->getEmployee($empNumber);
+                $this->fullName = $this->employee->getFullName();
+        }
+
+    }
+
+
+
+}
---
+++ symfony/plugins/orangehrmPimPlugin/modules/pim/templates/viewPrintDetailsSuccess.php
@@ -0,0 +1,12 @@
+
+<div id="profile-pic">
+</br>    
+<h1><?php echo $fullName; ?></h1>
+  <div class="imageHolder">
+    <img alt="Employee Photo" src="<?php echo url_for("pim/viewPhoto?empNumber=". $empNumber); ?>" border="0" id="empPic" 
+     width="200" height="200"/>
+  </a>
+  </div>    
+</div> 
+    
+</div> 
